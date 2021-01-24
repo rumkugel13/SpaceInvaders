b5b3e51a5c069df04dafc18ac488905a5ec7db13
rumkugel13 <rumkugel13@outlook.com>
2021-01-24 10:06:32 +0100

    Add web version

---
+++ SpaceInvaders.Web/App.cs
@@ -0,0 +1,32 @@
+using SpaceInvaders.Shared;
+using System;
+using System.IO;
+using static Retyped.dom;
+
+namespace SpaceInvaders.Web
+{
+    public class App
+    {
+        private static Game1 _game;
+        
+        public static void Main()
+        {
+            var canvas = new HTMLCanvasElement();
+            canvas.width = 800;
+            canvas.height = 480;
+            canvas.id = "monogamecanvas";
+
+            var button = new HTMLButtonElement();
+            button.innerHTML = "Run Game";
+
+            document.body.appendChild(canvas);
+            document.body.appendChild(button);
+
+            button.onclick = (ev) =>
+            {
+                _game = new Game1();
+                _game.Run();
+            };
+        }
+    }
+}
